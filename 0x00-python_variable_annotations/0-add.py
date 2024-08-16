14c3dd09a39fc006d7dcf965419d04dcf8d36807
adams miracle <adamsmiracle0@gmail.com>
2024-08-16 21:02:31 +0000

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+
+        Python basic annotations
+
+    """
+
+
+def add(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: The sum of the two inputs
+    """
+
+    return a + b

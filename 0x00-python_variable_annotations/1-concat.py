cb6d584fad4fabd4f6cc2bc67121df9bde37374d
adams miracle <adamsmiracle0@gmail.com>
2024-08-16 21:15:01 +0000

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+annotated funtion that concatenates string
+"""
+
+
+def concat(string1: str, string2: str) -> str:
+    """_summary_
+
+    Args:
+        string1 (str): first string
+        string2 (str): second string
+
+    Returns:
+        str: the concatenated string
+    """
+    return string1 + string2

61cf567974750cd5be3dfe6a4873d98b28cccef4
adams miracle <adamsmiracle0@gmail.com>
2024-08-16 21:23:50 +0000

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import math
+
+"""floor function in python
+"""
+
+
+def floor(n: float) -> int:
+    """_summary_
+
+    Args:
+        n (float): input float number
+
+    Returns:
+        int: returned floor number
+    """
+
+    return math.floor(n)

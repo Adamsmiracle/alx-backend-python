b72f38691dc54ae07088af6ba21f7bb0aa424678
adams miracle <adamsmiracle0@gmail.com>
2024-08-16 21:46:20 +0000

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+"""calculate the sum of list of float
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum of floats
+
+    Args:
+        input_list (List[float]): list of floats
+
+    Returns:
+        float: sum of list
+    """
+
+    sum: float = 0
+    for i in sum_list:
+        sum += i
+    return sum
